795274abefd622fc486b766d96d7ba4f30e6f247
ntuanh <ntuanh2705@gmail.com>
2024-12-29 18:47:52 +0700

    [ADD] add cursor handle

--- examples/LineExample.c
+++ examples/LineExample.c
@@ -1,12 +1,31 @@
-#include<windows.h>
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdbool.h>
 
-#include"../src/shapes/line.h"
 
+#include "../src/shapes/line.h"
 
+bool checkout() {
+    char c = getch(); 
+    return (c == 'q' || c == 'Q');
+}
 
-int main()
-{
-    testLines();
-    Sleep(5000);
+int main() {
+    int count = 0;
+    while (!checkout()) {
+        drawLine(10 , 10 , 20 , 20 , '-');
+        Sleep(2000);
+    }
     return 0;
 }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     printf("\033[1;31mThis is red text.\033[0m\n");
+//     printf("\033[1;32mThis is green text.\033[0m\n");
+//     Sleep(3000);
+//     return 0;
+// }
