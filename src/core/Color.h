14375a987246a5c4b162e02f10a04a5eeaa26e68
ntuanh <ntuanh2705@gmail.com>
2024-12-29 21:26:34 +0700

    [ADD] libraries

---
+++ src/core/Color.h
@@ -0,0 +1,10 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+void setColor(const char* colorName) ;
+
+#endif
---
+++ src/core/Cursor.h
@@ -0,0 +1,9 @@
+#ifndef CURSOR_H
+#define CURSOR_H
+
+#include<stdio.h>
+#include<windows.h>
+
+void setCursorPosition(int x, int y);
+
+#endif
